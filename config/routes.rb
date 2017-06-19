b01e94a5db3e83de9b24522118261191e2f7e8e1
Roopesh <rrattan.89@gmail.com>
2017-06-19 10:11:36 -0400

    creating user sign up model controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   delete 'pictures/:id' => 'pictures#destroy'
 
+  post 'users' => 'users#create'
+  get 'users/new' => 'users#new'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607204901) do
+ActiveRecord::Schema.define(version: 20170619134526) do
 
   create_table "pictures", force: :cascade do |t|
     t.string   "artist"
     t.string   "title"
     t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
