378445a673966570c20f75e91ee3a9524a015c36
Roopesh <rrattan.89@gmail.com>
2017-06-19 10:25:29 -0400

    sign up form controller + methods created & filled

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,11 +1,20 @@
 class UserController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to "/pictures"
+    else
+      render :new
+    end
+  end
 
+  def user_params
+    {email: params[:user][:email], password: params[:user][:password], password_confirmation: params[:user][:password_confirmation]}
   end
 
 end
